2dc8611e8053a6f5193eb877d546152df2d5f4bf
王瑜 <wangyu1455@jd.com>
2021-03-02 12:21:23 +0800

    Construct project structure.

---
+++ constant/crawler.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+"""The constants for crawler."""
+
+URL = "http://search.ccgp.gov.cn/bxsearch"
+KEY_WORDS = ["社区", "智能社区", "智慧社区", "商业街", "商圈", "智能商圈", "步行街", "景区", "智能景区",
+             "智慧景区", "文旅", "机场", "智慧文旅"]
---
+++ constant/settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+"""Default value for settings."""
+
+DB_TYPE_MONGO = "MONGO"
+
+ENV_FILE = ".env"
+ENV_PREFIX = "UCUC_"
