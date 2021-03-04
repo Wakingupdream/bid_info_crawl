f105d5f3b702604ea29826da9adb8e6e5de8cf25
wangyu1455 <wangyu1455@jd.com>
2021-03-04 14:01:11 +0800

    Modify constant/crawler.py; Finish get_page_num and get_page_data function.

--- constant/crawler.py
+++ constant/crawler.py
@@ -2,6 +2,31 @@
 # !/usr/bin/env python
 """The constants for crawler."""
 
+import datetime
+
+
+START_TIME = datetime.datetime.now().strftime("%Y:%m:%d")
+END_TIME = datetime.datetime.now().strftime("%Y:%m:%d")
+INFO_NAME = ["page_url", "project_name", "bid_type", "issue_time", "buyer",
+             "agency", "province"]
 URL = "http://search.ccgp.gov.cn/bxsearch"
-KEY_WORDS = ["社区", "智能社区", "智慧社区", "商业街", "商圈", "智能商圈", "步行街", "景区", "智能景区",
-             "智慧景区", "文旅", "机场", "智慧文旅"]
+KEY_WORDS = ["社区", "智能社区", "智慧社区", "商业街", "商圈", "智能商圈", "步行街",
+             "景区", "智能景区", "智慧景区", "文旅", "机场", "智慧文旅"]
+PARAMETER = params_l = {
+    "searchtype": 1,
+    "page_index": 1,
+    "bidSort": 0,
+    "buyerName": None,
+    "projectId": None,
+    "pinMu": 0,
+    "bidType": 0,
+    "dbselect": "bidx",
+    "kw": None,
+    "start_time": START_TIME,  # TODO(wangyu/crawl_code):Need modify.
+    "end_time": END_TIME,
+    "timeType": 5,
+    "displayZone": None,
+    "zoneId": None,
+    "pppStatus": 0,
+    "agentName": None,
+}
--- uc_crawler/utils.py
+++ uc_crawler/utils.py
@@ -1,3 +1,90 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
 """Utilities for uc-crawler-ccgp."""
+
+import re
+import sys
+import time
+
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+from constant import crawler
+from utils.log import LOG
+
+
+def get_page_num(params):  # TODO:注意调用的时候参数及位置,params 要添加key_word
+    """Get total page num according to specific keyword."""
+    ua_l = UserAgent()
+    ss_l = requests.session()
+    ss_l.headers["User-Agent"] = ua_l.random
+    params_l = params
+    url_l = crawler.URL
+    res_l = ss_l.get(url_l, params=params_l)
+    soup_l = BeautifulSoup(res_l.text, "html.parser")
+    if soup_l.find("p", attrs={"class": "pager"}) is None:
+        LOG.info(f"{params['kw']}页数为空")
+        sys.exit()
+    page_num = re.search("size:(.*),",
+                         soup_l.find("p", attrs={"class": "pager"}).find(
+                             "script").next)[1]
+    return int(page_num)
+
+
+def re_connect(elem_bid_list, ss_l, params_l, page_index, sec=1):
+    """Reconnect when the page is not loaded because the visit is too fast."""
+    if elem_bid_list:
+        return elem_bid_list
+    if sec == 5:
+        ua_l = UserAgent()
+        ss_l = requests.session()
+        ss_l.headers["User-Agent"] = ua_l.random
+        sec = 1
+    res_l = ss_l.get(crawler.URL, params=params_l)
+    time.sleep(sec)
+    LOG.info(res_l, page_index)
+    soup = BeautifulSoup(res_l.text, "html.parser")
+    elem_bid_list = soup.find("ul", attrs={"class": "vT-srch-result-list-bid"})
+    return re_connect(elem_bid_list, ss_l, params_l, page_index, sec + 1)
+
+
+def get_page_data(ua_l, ss_l, params, page_index):  # TODO:注意调用的时候参数及位置
+    """
+    Extract page info from key word searching result.
+
+    Page info include page_url, project_name, bid_type, issue_time, buyer,
+    agency, province.
+    """
+    info_dic = {}
+    for name in crawler.INFO_NAME:
+        info_dic[name] = []
+
+    params_l = params
+    params_l["page_index"] = page_index
+    ss_l.headers["User-Agent"] = ua_l.random
+    url = crawler.URL
+
+    res_l = ss_l.get(url, params=params_l)
+    LOG.info(res_l, page_index)
+    soup = BeautifulSoup(res_l.text, "html.parser")
+    elem_bid_list = soup.find("ul", attrs={"class": "vT-srch-result-list-bid"})
+    if elem_bid_list is None:
+        elem_bid_list = re_connect(elem_bid_list, ss_l, params_l, page_index,
+                                   sec=2)
+    li_list = elem_bid_list.find_all("li")
+    try:
+        for elem_bid in li_list:
+            info_dic["page_url"].append(elem_bid.a["href"])
+            info_dic["bid_type"].append(elem_bid.span.strong.text.split())
+            info_dic["project_name"].append(
+                "".join(elem_bid.find_all("a")[0].text.strip()))
+            i_b_a_p = elem_bid.span.text.split("|")[:4]
+            info_dic["issue_time"].append(i_b_a_p[0].strip())
+            info_dic["buyer"].append(
+                re.search("采购人：(.*)", i_b_a_p[1].strip()).group(1))
+            info_dic["agency"].append(
+                re.search("代理机构：(.*)\r", i_b_a_p[2].strip()).group(1))
+            info_dic["province"].append(i_b_a_p[3].strip())
+    except ValueError:
+        LOG.error("li_list is None.")
